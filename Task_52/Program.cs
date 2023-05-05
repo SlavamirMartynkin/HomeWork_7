267711de6bdb2718e7194153b1f66924f7dbeb69
Martynkin Slavamir <sss_mmm86@mail.ru>
2023-05-05 18:35:01 +0300

    add Task_52

---
+++ Task_52/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] FillFrrayMatrix(int[,] matr) // заполняет матрицу случайными числами
+{
+    Random rnd =new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = rnd.Next(0, 2);
+        }
+    }
+    return matr;
+}
+void PrintArrayMatrix(int[,] matr)  // выводит матрицу на экран
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+void SrAr(int[,] matr)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            sum += matr[i,j];
+        }
+        //double srar = sum / matr.GetLength(0);
+        System.Console.Write($"({Math.Round(sum / (double)matr.GetLength(0), 1)}) ");
+    }
+}
+
+int[,] matrix = new int[2, 5];
+matrix = FillFrrayMatrix(matrix);
+PrintArrayMatrix(matrix);
+System.Console.WriteLine();
+SrAr(matrix);
